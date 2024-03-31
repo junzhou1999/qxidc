55378812704e749e98144d96cad53141fc6585ca
junzhou1999 <123@qq.com>
2024-03-31 12:02:55 +0800

    创建网络服务socket，同时提前做好备份，要迭代的文件更高

---
+++ demo_socket/book03.cpp
@@ -0,0 +1,83 @@
+/**
+ * book03.cpp：本程序用于socket服务端的实现
+*/
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+int main(int argc, char* argv[])
+{
+  if (argc!=2)
+  {
+    printf("Using: book01 port\n");
+    printf("Example: ./book01 5005\n\n");
+    return -1;
+  }
+
+  // 创建socket
+  int listenfd;
+  if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("socket");
+    return -1;
+  }
+
+  // 配置连接参数，绑定
+  struct sockaddr_in serveraddr;
+  memset(&serveraddr, 0, sizeof(serveraddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  // serveraddr.sin_addr.s_addr = inet_addr("118.89.50.198"); // 指定ip地址。
+  serveraddr.sin_port = htons(atoi(argv[1]));
+  if (bind(listenfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) !=0 )
+  {
+   perror("bind");
+   return -1; 
+  }
+
+  // sockets设置为监听模式，参数3为可以容纳的established队列个数-1
+  if (listen(listenfd, 3) != 0 )
+  {
+    perror("listen");
+    return -1;
+  }
+
+  // listen socket阻塞等待，可以从这里分叉了
+  int clientfd;
+  struct sockaddr_in clientaddr;
+  int addrlen = sizeof(clientaddr);
+  clientfd = accept(listenfd, (struct sockaddr*)&clientaddr, (socklen_t*)&addrlen);
+  printf("客户端（%s）已连接。\n", inet_ntoa(clientaddr.sin_addr));
+
+  // 双方开始通信
+  char buffer[1024];
+  while(true)  // 服务端接收来自客户端的交流请求
+  {
+    memset(buffer, 0, sizeof(buffer));
+    int iret;
+    if ((iret = recv(clientfd, buffer, 1024, 0)) <= 0)
+    {
+      printf("iret=%d\n", iret);
+      break;
+    }
+    printf("iret=%d，接收：%s\n", iret, buffer);
+
+    strcpy(buffer, "ok");
+    if ((iret=send(clientfd, buffer, strlen(buffer), 0)) <= 0)
+    {
+      perror("send");
+      break;
+    }
+    printf("iret=%d，发送：%s\n", iret, buffer);
+
+  }
+
+  close(listenfd);
+  close(clientfd);
+  return 0;
+}
+
---
+++ demo_socket/book04.cpp
@@ -0,0 +1,81 @@
+/**
+ * book04.cpp：本程序用于socket客户端的实现
+*/
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+  if (argc!=3)
+  {
+    printf("Using: book02 ip port\n");
+    printf("Example: ./book02 192.168.0.74 5005\n\n");
+    return -1;
+  }
+
+  // 创建socket
+  int socketfd;
+  if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("socket");
+    return -1;
+  }
+
+  // 配置服务器信息，连接
+  struct sockaddr_in serveraddr;
+  memset(&serveraddr, 0, sizeof(serveraddr));
+  int addrlen = sizeof(serveraddr);
+  serveraddr.sin_family = AF_INET;  // 协议簇
+  serveraddr.sin_port = htons(atoi(argv[2]));  // 端口
+  struct hostent* h = NULL;
+  if ((h=gethostbyname(argv[1])) == NULL)
+  {
+    printf("gethostbyname(%s) failed.\n", argv[1]);
+    return -1;
+  }
+  memcpy(&serveraddr.sin_addr, h->h_addr, h->h_length);  // IP地址
+
+  // 主动连接
+  if (connect(socketfd, (struct sockaddr*)&serveraddr, (socklen_t)addrlen) != 0)
+  {
+    perror("connect");
+    close(socketfd);
+    return -1;
+  }
+
+  // 通信
+  char buffer[1024];
+  for (int ii=0;ii<100000000;ii++)
+  {
+    memset(buffer, 0, sizeof(buffer));
+    sprintf(buffer, "这是第%d个超女，编号：%010d。", ii+1, ii);
+    int iret;
+    if ((iret=send(socketfd, buffer, 1024, 0)) <= 0)
+    {
+      perror("send");
+      break;
+    }
+    printf("iret=%d，发送：%s\n", iret, buffer);
+
+sleep(1);
+
+    if ((iret = recv(socketfd, buffer, sizeof(buffer), 0)) <= 0)
+    {
+      printf("iret=%d\n", iret);
+      break;
+    }
+    printf("iret=%d, 接收：%s\n", iret, buffer);
+
+  }
+
+  close(socketfd);
+  return 0;
+}
+
+
